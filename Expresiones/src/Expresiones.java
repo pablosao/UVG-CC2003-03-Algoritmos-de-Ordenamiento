447a352cb5a53279224f3a624939dc3f3a9b9220
Pablo Sao <pablo_sao@outlook.com>
2019-02-03 11:17:34 -0600

    optimizando código

--- Expresiones/src/Expresiones.java
+++ Expresiones/src/Expresiones.java
@@ -1,8 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 
 /**
  *
@@ -10,12 +9,44 @@
  * @date 02/01/2018
  */
 public class Expresiones {
-
+    
+    private static int REPETICION = 10;
+    private static String PATH_FILE = "./numeros.txt";
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //iniciando la escritura de los números al azar
+        System.out.println(String.format("Iniciando la escritura de los %d dígitos.", REPETICION));
+        
+        try{
+            
+            FileWriter escritura = new FileWriter (new File(PATH_FILE));
+            BufferedWriter bw = new BufferedWriter(escritura);
+            
+            int numero=10;
+            
+            for(int i = 0; i < numero; i++){
+                
+                int aleatorio = (int)(Math.random()*3000);
+                escritura.write(String.valueOf(aleatorio));
+                escritura.write("\n");
+             
+            }
+            
+            escritura.close();
+            
+            System.out.println(String.format("Se ha finalizado la escritura de los %d dígitos.", REPETICION));
+            
+        }catch(Exception e){
+            
+            System.out.println(e.toString()); 
+        
+        }
+        
+        
     }
     
 }
