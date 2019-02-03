1d854290b9a9ca904f2565bf8eee360a671d3f90
Pablo Sao <pablo_sao@outlook.com>
2019-02-03 14:43:01 -0600

    Optimizando código

--- Expresiones/src/Expresiones.java
+++ Expresiones/src/Expresiones.java
@@ -1,7 +1,10 @@
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Arrays;
 
 /**
  *
@@ -10,9 +13,8 @@ import java.io.FileWriter;
  */
 public class Expresiones {
     
-    private static int REPETICION = 10;
+    private static int REPETICION = 3000;
     private static String PATH_FILE = "./numeros.txt";
-    
     /**
      * @param args the command line arguments
      */
@@ -21,32 +23,120 @@ public class Expresiones {
         //iniciando la escritura de los números al azar
         System.out.println(String.format("Iniciando la escritura de los %d dígitos.", REPETICION));
         
+        //escribiendo libros en el archivo
+        setData();
+        
+        System.out.println(String.format("Se ha finalizado la escritura de los %d dígitos. ", REPETICION));
+        
+        System.out.println("\nObteniendo dígitos de archivo..");
+        
+        
+        
+        //Verificamos que hayan registros para realizar las operaciones
+        if(getData() != null){
+            int[] digitos = null; 
+            //Metodo Gnome sort
+            digitos = getData();
+            System.out.println("Arreglo generado: " + Arrays.toString(digitos));
+            System.out.println("Algoritmo Gnome sort: " + Arrays.toString(GnomeSort.gnomeSort(digitos)));
+
+
+            //Merge sort
+            digitos = getData();
+            System.out.println("\nArreglo generado: " + Arrays.toString(digitos));
+            System.out.println("Algoritmo Merge sort: " + Arrays.toString(MergeSort.mergesort(digitos)));
+
+
+             //Quick sort
+            digitos = getData();
+            System.out.println("\nArreglo generado: " + Arrays.toString(digitos));
+            QuickSort ob = new QuickSort(); 
+            ob.sort(digitos, 0, (digitos.length - 1)); 
+            System.out.println("Algoritmo Quick Sort: " + Arrays.toString(digitos));
+
+            //Radix Sort
+            digitos = getData();
+            System.out.println("\nArreglo generado: " + Arrays.toString(digitos));
+            System.out.println("Algoritmo Radix sort: " + Arrays.toString(RadixSort.radixsort(digitos,REPETICION)));
+
+            //Bubble short
+            digitos = getData();
+            System.out.println("\nArreglo generado: " + Arrays.toString(digitos));
+            BurbujaSort burbuja = new BurbujaSort();
+            System.out.println("Algoritmo Bubble sort: " + Arrays.toString(burbuja.burbuja(digitos)));
+        }
+        
+    }
+    
+    /**
+     * Metodo para generar los numeros configurados en la bariable REPETICION en el archivo ubicado en PATH_FILE
+     */
+    private static void setData(){
         try{
             
             FileWriter escritura = new FileWriter (new File(PATH_FILE));
-            BufferedWriter bw = new BufferedWriter(escritura);
             
-            int numero=10;
-            
-            for(int i = 0; i < numero; i++){
+            for(int control = 0; control < REPETICION; control++){
                 
-                int aleatorio = (int)(Math.random()*3000);
-                escritura.write(String.valueOf(aleatorio));
+                escritura.write(String.valueOf((int)(Math.random() * 3000 /*REPETICION*/)));
                 escritura.write("\n");
              
             }
             
             escritura.close();
-            
-            System.out.println(String.format("Se ha finalizado la escritura de los %d dígitos.", REPETICION));
-            
+                        
         }catch(Exception e){
             
             System.out.println(e.toString()); 
         
         }
+    }
+    
+    
+    private static int[] getData(){
+        int[] datos = new int[REPETICION];
         
+        try{
+            
+            File archivo = new File(PATH_FILE);
+            
+            //Verificamos si el archivo existe
+            if(archivo.exists()){
+                //Perarando archivo para lectura
+                BufferedReader lectura = new BufferedReader(new FileReader(archivo)); 
+            
+                //Variable que contendra el numero del archivo a leer
+                String numero;
+            
+                //control del correlativo del número
+                int control = 0;
+            
+                //ciclo para la lectura de los digitos contenidos en el archivo
+                while ((numero = lectura.readLine()) != null) {
+                    //asignacion del digito entero dentro de la variable datos
+                    datos[control] = Integer.parseInt(numero);
+                    //sumando 1 a la variable de control
+                    control++;
+
+                } 
+            }
+            else{
+                //Si el archivo no existe se muestra al usuario el path ingresado y se retorna nulo
+                System.out.println(String.format("El archivo \"%s\" no fue encontrado.",PATH_FILE));
+                datos = null;
+            }
+            
+        }
+        //Captura de cualquier tipo de error.
+        catch(Exception e){
+            //mostando que hay surgio un error al ejecutar el archivo
+            System.out.println(String.format("Problema al leer el archivo. \nError: %s",e.toString()));
+            
+            //Si existe un error se retornara un nulo
+            datos = null;
+        }
         
+        return datos;
     }
     
 }
--- Expresiones/src/QuickSort.java
+++ Expresiones/src/QuickSort.java
@@ -1,65 +1,65 @@
-/**
- * Código tomado del libro Java Structures
- * Bailey, D. (2007). Java Structures. Williams College.
- * 
- * @author Andrea Elias
- * @author Pablo Sao
- * @version 01/02/2019
- */
+class QuickSort 
+{ 
+    /* This function takes last element as pivot, 
+       places the pivot element at its correct 
+       position in sorted array, and places all 
+       smaller (smaller than pivot) to left of 
+       pivot and all greater elements to right 
+       of pivot */
+    int partition(int arr[], int low, int high) 
+    { 
+        int pivot = arr[high];  
+        int i = (low-1); // index of smaller element 
+        for (int j=low; j<high; j++) 
+        { 
+            // If current element is smaller than or 
+            // equal to pivot 
+            if (arr[j] <= pivot) 
+            { 
+                i++; 
+  
+                // swap arr[i] and arr[j] 
+                int temp = arr[i]; 
+                arr[i] = arr[j]; 
+                arr[j] = temp; 
+            } 
+        } 
+  
+        // swap arr[i+1] and arr[high] (or pivot) 
+        int temp = arr[i+1]; 
+        arr[i+1] = arr[high]; 
+        arr[high] = temp; 
+  
+        return i+1; 
+    } 
+  
+  
+    /* The main function that implements QuickSort() 
+      arr[] --> Array to be sorted, 
+      low  --> Starting index, 
+      high  --> Ending index */
+    void sort(int arr[], int low, int high) 
+    { 
+        if (low < high) 
+        { 
+            /* pi is partitioning index, arr[pi] is  
+              now at right place */
+            int pi = partition(arr, low, high); 
+  
+            // Recursively sort elements before 
+            // partition and after partition 
+            sort(arr, low, pi-1); 
+            sort(arr, pi+1, high); 
+        } 
+    } 
+  
+    /* A utility function to print array of size n */
+    static void printArray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i=0; i<n; ++i) 
+            System.out.print(arr[i]+" "); 
+        System.out.println(); 
+    } 
 
-public class QuickSort {
-     
-    private int array[];
-    private int length;
- 
-    public void sort(int[] inputArr) {
-         
-        if (inputArr == null || inputArr.length == 0) {
-            return;
-        }
-        this.array = inputArr;
-        length = inputArr.length;
-        quickSort(0, length - 1);
-    }
- 
-    private void quickSort(int lowerIndex, int higherIndex) {
-         
-        int i = lowerIndex;
-        int j = higherIndex;
-        // calculate pivot number, I am taking pivot as middle index number
-        int pivot = array[lowerIndex+(higherIndex-lowerIndex)/2];
-        // Divide into two arrays
-        while (i <= j) {
-            /**
-             * In each iteration, we will identify a number from left side which
-             * is greater then the pivot value, and also we will identify a number
-             * from right side which is less then the pivot value. Once the search
-             * is done, then we exchange both numbers.
-             */
-            while (array[i] < pivot) {
-                i++;
-            }
-            while (array[j] > pivot) {
-                j--;
-            }
-            if (i <= j) {
-                exchangeNumbers(i, j);
-                //move index to next position on both sides
-                i++;
-                j--;
-            }
-        }
-        // call quickSort() method recursively
-        if (lowerIndex < j)
-            quickSort(lowerIndex, j);
-        if (i < higherIndex)
-            quickSort(i, higherIndex);
-    }
- 
-    private void exchangeNumbers(int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-}
-     
+} 
