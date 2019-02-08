9e2da198d85ad856d3d2fbd0fe9d7419ba908aa5
Pablo Sao <pablo_sao@outlook.com>
2019-02-08 12:13:42 -0600

    Implementando clase Comparable en burbujaSort

--- Expresiones/src/BurbujaSort.java
+++ Expresiones/src/BurbujaSort.java
@@ -8,20 +8,30 @@
  * @version 01/02/2019
  */
 
-public class BurbujaSort 
+public class BurbujaSort implements Comparable<DatosNumericos>
 {
     
+    DatosNumericos numero;
     public int[] burbuja(int [] A)
     {
-         int i, j, aux;
+         int i, j = 0, aux;
          for(i=0;i<A.length-1;i++)
               for(j=0;j<A.length-i-1;j++)
-                   if(A[j+1]<A[j]){
+                  //Implementando comparable
+                  numero = new DatosNumericos(A[j+1],A[j]);
+         
+                   //if(A[j+1]<A[j]){
+                    if(compareTo(numero) == 1){
                       aux=A[j+1];
                       A[j+1]=A[j];
                       A[j]=aux;
                    }
     return A;
+    }
+
+    @Override
+    public int compareTo(DatosNumericos o) {
+        return o.dato1 < o.dato2? 1:0;
     }
     
     
